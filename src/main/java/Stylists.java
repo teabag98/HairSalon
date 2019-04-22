ad598d61eafd37b4589dc150f37e578aef2e90dc
eric <eric.kingoo2014@gmail.com>
2019-04-23 00:33:29 +0300

    add stylists section

--- src/main/java/Stylists.java
+++ src/main/java/Stylists.java
@@ -1,39 +1,81 @@
 import java.util.List;
+
 import org.sql2o.*;
 
 public class Stylists {
     private String name;
-    private  String description;
+    private String description;
     private int id;
 
 
     @Override
-    public boolean equals(Object otherStylists){
-        if(!(otherStylists instanceof Stylists)){
-            return  false;
-        }
-        else {
-         Stylists newStylist = (Stylists) otherStylists;
-         return  this.getName().equals(newStylist.getName())&&
-                  this.getDescription().equals(newStylist.getDescription())                                        &&
-                 this.getId()==newStylist.getId();
+    public boolean equals(Object otherStylists) {
+        if (!(otherStylists instanceof Stylists)) {
+            return false;
+        } else {
+            Stylists newStylist = (Stylists) otherStylists;
+            return this.getName().equals(newStylist.getName()) &&
+                    this.getDescription().equals(newStylist.getDescription()) &&
+                    this.getId() == newStylist.getId();
         }
     }
 
-    public Stylists(String name, String description){
+    public Stylists(String name, String description) {
         this.name = name;
         this.description = description;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public  String getDescription(){
+    public String getDescription() {
         return description;
     }
-    public int getId(){
+
+    public int getId() {
         return id;
     }
 
+    public void save() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO stylists (name,description)VALUES(:name,:description)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .executeUpdate()
+                    .getKey();
+
+        }
+
+    }
+
+    public static List<Stylists> all() {
+        String sql = "SELECT id,name,description FROM stylists";
+        try (Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql)
+                    .executeAndFetch(Stylists.class);
+        }
+    }
+
+    public List<Clients> getClients() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM stylists where stylistId=:id";
+            return con.createQuery(sql)
+                    .addParameter("id", this.id)
+                    .executeAndFetch(Clients.class);
+        }
+
+
+    }
+
+    public static Stylists find(int id){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "SELECT * FROM stylists where id =:id";
+            Stylists stylist= con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Stylists.class);
+            return stylist;
+
+        }
+    }
 }
--- src/test/java/Stylists_test.java
+++ src/test/java/Stylists_test.java
@@ -3,6 +3,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class Stylists_test {
@@ -24,10 +27,65 @@ public class Stylists_test {
     }
 
     @Test
-    public void equals_rerurnsTrueIfNamesAreTheSame(){
+    public void equals_returnsTrueIfNamesAreTheSame(){
 Stylists firstStylist = new Stylists("eric","supertalented");
         Stylists secondStylist = new Stylists("eric","supertalented");
         assertTrue(firstStylist.equals(secondStylist));
+
+    }
+
+    @Test
+    public void save_addsNewStylistToTheDatabase(){
+        Stylists myStylist = new Stylists("mary","always smiling");
+        myStylist.save();
+        assertTrue(Stylists.all().get(0).equals(myStylist));
+     }
+
+     @Test
+    public void all_returnsAllInstancesOfStylists(){
+        Stylists firstStylist = new Stylists("Tatu","cool");
+        firstStylist.save();
+        Stylists secondStylist = new Stylists("vilan","fastest worker");
+        secondStylist.save();
+        assertEquals(true,Stylists.all().get(0).equals(firstStylist));
+    assertEquals(true, Stylists.all().get(1).equals(secondStylist));
+     }
+
+    @Test
+    public void getName_categoryInstantiatesWithName_Home() {
+        Stylists myStylist = new Stylists("ric", "best-barber");
+        assertEquals("ric", myStylist.getName());
+    }
+
+    @Test
+    public void getId_assignsIdToObject() {
+        Stylists myStylist = new Stylists("kiki","awesome girl");
+        myStylist.save();
+
+    }
+
+    @Test
+    public void getClients_retrievesAllTasksFromDatabase_TasksList(){
+        Stylists myStylist = new Stylists("Ho","crazy");
+        myStylist.save();
+        Clients firstClient = new Clients("Mow", myStylist.getId());
+        firstClient.save();
+        Clients secondClient = new Clients("Mow", myStylist.getId());
+        secondClient.save();
+
+        Clients[] client = new Clients[] {firstClient,secondClient};
+
+        assertTrue(myStylist.getClients().containsAll(Arrays.asList(client)));
+
+    }
+
+    @Test
+    public void find_returnsCategoryWithSameId_secondCategory() {
+        Stylists firstStylist = new Stylists("cuty","blak eyes");
+        firstStylist.save();
+        Stylists secondStylist = new Stylists("gorz", "tall");
+        secondStylist.save();
+        assertEquals(Stylists.find(secondStylist.getId()),secondStylist);
 
     }
 }
